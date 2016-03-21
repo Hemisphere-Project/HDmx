3b6e5ee3ae94ebf49fdebdbd52e9d42057b330e1
maigre <thomas@hmsphr.com>
2016-03-21 18:19:38 +0100

    Reclean

--- DmxPy.py
+++ DmxPy.py
@@ -6,7 +6,7 @@ import sys
 
 DMXOPEN = chr(126)
 DMXCLOSE = chr(231)
-DMXINTENSITY=chr(6)+chr(1)+chr(2)				
+DMXINTENSITY=chr(6)+chr(1)+chr(2)
 DMXINIT1= chr(03)+chr(02)+chr(0)+chr(0)+chr(0)
 DMXINIT2= chr(10)+chr(02)+chr(0)+chr(0)+chr(0)
 
@@ -19,24 +19,24 @@ class DmxPy:
 			sys.exit(0)
 		self.serial.write( DMXOPEN+DMXINIT1+DMXCLOSE)
 		self.serial.write( DMXOPEN+DMXINIT2+DMXCLOSE)
-		
 		self.dmxData=[chr(0)]*513   #128 plus "spacer".
-		
+
 	def setChannel(self, chan, intensity):
 		if chan > 512 : chan = 512
 		if chan < 0 : chan = 0
 		if intensity > 255 : intensity = 255
 		if intensity < 0 : intensity = 0
 		self.dmxData[chan] = chr(intensity)
-		
+
+
 	def blackout(self):
 		for i in xrange (1, 512, 1):
 			self.dmxData[i] = chr(0)
-			
+
 	def full(self):
 		for i in xrange (1, 512, 1):
 			self.dmxData[i] = chr(255)
-		
+
 	def render(self):
 		sdata=''.join(self.dmxData)
 		self.serial.write(DMXOPEN+DMXINTENSITY+sdata+DMXCLOSE)
